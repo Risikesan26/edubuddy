5effcecb92a09f2824dec061975a54c32c766ca9
Risikesan <60293832+Risikesan26@users.noreply.github.com>
2025-08-10 14:32:55 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://your-vercel-app.vercel.app'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'https://edubuddy-wheat.vercel.app/'],
   credentials: true
 }));
 app.use(express.json());
